ae821e2fdd8441d479199bf2bcfb195baae920f7
realderpynarwhal <realderpynarwhal@gmail.com>
2025-05-21 17:14:10 -0400

    ignite and retract! now work together!

--- src/main/java/net/derpy/mod/DerpyslightsabersClient.java
+++ src/main/java/net/derpy/mod/DerpyslightsabersClient.java
@@ -3,14 +3,11 @@ package net.derpy.mod;
 import net.derpy.mod.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 import software.bernie.geckolib.GeckoLib;
-import software.bernie.geckolib.animatable.GeoItem;
 
 public class DerpyslightsabersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         GeckoLib.initialize();
         ModItems.ANAKINS_LIGHTSABER.getRenderProvider().get();
-
-
     }
 }
