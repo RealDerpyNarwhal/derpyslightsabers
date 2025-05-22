ce2a2883e4d25431025d012b9cdd2a0ea648123c
alexw <alex.j.witkowski@gmail.com>
2025-05-21 20:54:09 -0400

    Add animation support

--- src/main/java/net/derpy/mod/item/custom/AnakinsLightsaber.java
+++ src/main/java/net/derpy/mod/item/custom/AnakinsLightsaber.java
@@ -1,12 +1,32 @@
 package net.derpy.mod.item.custom;
 
+import bond.thematic.api.callbacks.SwingHandCallback;
+import bond.thematic.api.network.AnimationPacketHandler;
+import bond.thematic.api.network.packet.S2CAnimPacket;
+import bond.thematic.api.registries.anims.AnimationManager;
+import bond.thematic.api.registries.armors.ability.ThematicAbility;
+import bond.thematic.api.registries.data.EntityComponents;
+import bond.thematic.api.registries.item.IAttackUse;
+import bond.thematic.mod.entity.thrown.OrbEntity;
+import com.funalex.themAnim.api.layered.AnimationStack;
+import com.funalex.themAnim.api.layered.IAnimation;
+import com.funalex.themAnim.api.layered.KeyframeAnimationPlayer;
+import com.funalex.themAnim.api.layered.ModifierLayer;
+import com.funalex.themAnim.api.layered.modifier.AbstractModifier;
+import com.funalex.themAnim.core.data.KeyframeAnimation;
+import com.funalex.themAnim.minecraftApi.PlayerAnimationAccess;
+import com.funalex.themAnim.minecraftApi.PlayerAnimationRegistry;
+import net.derpy.mod.Derpyslightsabers;
 import net.derpy.mod.item.client.AnakinsLightsaberRenderer;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtByte;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
@@ -20,10 +40,13 @@ import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.RenderUtils;
 import software.bernie.geckolib.core.animation.RawAnimation;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class AnakinsLightsaber extends Item implements GeoItem {
+import static bond.thematic.api.network.AnimationPacketHandler.applyModifiers;
+
+public class AnakinsLightsaber extends Item implements GeoItem, IAttackUse {
     private static final RawAnimation IGNITE_ANIM = RawAnimation.begin().then("ignite", Animation.LoopType.HOLD_ON_LAST_FRAME);
     private static final RawAnimation RETRACT_ANIM = RawAnimation.begin().then("retract", Animation.LoopType.HOLD_ON_LAST_FRAME);
 
@@ -100,5 +123,28 @@ public class AnakinsLightsaber extends Item implements GeoItem {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+    @Override
+    public boolean attackUse(World world, PlayerEntity playerEntity, Hand hand) {
+        if (world.isClient) {
+            // In the future, Thematic will have support to just do:
+            // AnimationManager.triggerAnimation(Derpyslightsabers.MOD_ID, "swing_saber");
+            // but for now we'll do it this way because I forgot I hard-coded in the names
+
+            AnimationStack animationStack = PlayerAnimationAccess.getPlayerAnimLayer((AbstractClientPlayerEntity) playerEntity);
+
+            if (animationStack != null) {
+                // I'd recommend having multiple of these animations and using Math.random to choose random ones
+                // for each swing, that way you can have variation to it
+                // Also make sure the playerEntity.getItemCooldownManager() isn't up before you fire these animations, make sure they're the same length
+                // That way you don't over play the animation at all
+                Identifier animationLocation = Objects.requireNonNull(Identifier.of(Derpyslightsabers.MOD_ID, "swing_saber"));
+                KeyframeAnimation animation = PlayerAnimationRegistry.getAnimation(animationLocation);
+                KeyframeAnimationPlayer player = new KeyframeAnimationPlayer(Objects.requireNonNull(animation));
+                animationStack.addAnimLayer(5, player);
+            }
+        }
+        return true;
     }
 }
